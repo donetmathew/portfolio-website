ce5db51685c304a266c54e211d1ccf4d5a3ae9db
Donet Mathew <donetmathew@Donets-MBP.attlocal.net>
2024-12-30 01:16:25 -0500

    Removed typed.js library and updated fonts

--- portfolio/src/app/shared/typed/typed.component.ts
+++ portfolio/src/app/shared/typed/typed.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
-import Typed from 'typed.js';
 
 
 @Component({
@@ -15,54 +14,50 @@ export class TypedComponent implements OnInit {
   public currentIndex = 0;
   public isDeleting: Boolean = false;
   public txt = "";
-  // public typed = new Typed('#element', {
-  //   strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-  //   typeSpeed: 50,
-  // });
 
   constructor(private renderer: Renderer2, private cd:ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    const typed=new Typed('#element', {
-        strings: this.config.words,
-        typeSpeed: 70,
-        loop: true,
-        showCursor: false,
-       });
-    //this.type();
+    // const typed=new Typed('#element', {
+    //     strings: this.config.words,
+    //     typeSpeed: 70,
+    //     loop: true,
+    //     showCursor: false,
+    //    });
+    this.type();
 
   }
-  // type() {
+  type() {
 
-  //   var currentWord = this.config.words[this.currentIndex];
-  //   let delay = this.config.timer;
+    var currentWord = this.config.words[this.currentIndex];
+    let delay = this.config.timer;
 
-  //   if (this.isDeleting) {
-  //     this.txt = currentWord.substring(0, this.txt.length - 1);
-  //   }
-  //   else {
-  //     this.txt = currentWord.substring(0, this.txt.length + 1)
-  //   }
+    if (this.isDeleting) {
+      this.txt = currentWord.substring(0, this.txt.length - 1);
+    }
+    else {
+      this.txt = currentWord.substring(0, this.txt.length + 1)
+    }
 
-  //   if (!this.isDeleting && this.txt === currentWord) {
-  //     delay = 2000;
-  //     this.isDeleting = true;
-  //   }
+    if (!this.isDeleting && this.txt === currentWord) {
+      delay = 2000;
+      this.isDeleting = true;
+    }
 
-  //   else if (this.isDeleting && this.txt == "") {
-  //     this.isDeleting = false;
-  //     this.currentIndex++;
-  //     if (this.currentIndex > this.config.words.length - 1) {
-  //       this.currentIndex = 0;
-  //     }
-  //   }
-  //   else if (this.isDeleting) {
-  //     delay /= 4;
-  //   }
+    else if (this.isDeleting && this.txt == "") {
+      this.isDeleting = false;
+      this.currentIndex++;
+      if (this.currentIndex > this.config.words.length - 1) {
+        this.currentIndex = 0;
+      }
+    }
+    else if (this.isDeleting) {
+      delay /= 4;
+    }
 
-  //   setTimeout(() => {
-  //     this.type();
-  //     this.cd.detectChanges();
-  //   }, delay)
-  // }
+    setTimeout(() => {
+      this.type();
+      this.cd.detectChanges();
+    }, delay)
+  }
 }
